b8e02509a4273ced6f5f12db937722d356e15f2d
“alihusnain562003” <alihusnainkashif3@gmail.com>
2024-09-15 16:54:25 +0500

    mark sheet

--- laravelkam2/resources/views/marksheet.blade.php
+++ laravelkam2/resources/views/marksheet.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,10 +10,42 @@
 
 <body>
 
-    <h1>{{ $name }}</h1>
-    <h1>{{ $id }}</h1>
 
- 
+    <h1>{{ $name }}</h1>
+
+    <h2>English : {{ $english }}</h2>
+    <h2>Urdu : {{ $urdu }}</h2>
+    <h2>math : {{ $maths }}</h2>
+
+
+    @php
+        $obtained = $english + $urdu + $maths;
+
+        $per = ($obtained / 300) * 100;
+    @endphp
+
+    <h1>
+       Obtained Marks  {{ $obtained }}
+
+    </h1>
+
+  <h1> Percentage  {{ $per }} %</h1>
+
+    @if ($per <= 100 && $per >= 80)
+        {{ 'grade A1' }}
+    @elseif ($per <= 79 && $per >= 70)
+        {{ 'grade A' }}
+    @elseif ($per <= 69 && $per >= 60)
+        {{ 'grade B' }}
+    @elseif ($per <= 59 && $per >= 50)
+        {{ 'grade C' }}
+    @elseif ($per <= 49 && $per >= 40)
+        {{ 'grade D' }}
+    @else
+        {{ 'Your are Faild ' }}
+    @endif
+
+
 
 
 
--- laravelkam2/routes/web.php
+++ laravelkam2/routes/web.php
@@ -19,25 +19,11 @@ Route::get('/month', function () {
     return view('month');
 });
 
-Route::get('/marksheet/{username}/{userid?}/{english}/{urdunumber}/{mathsnumber}', function ($username, $userid = null, $english, $urdu, $maths) {
-    $english = (int)$english;
-    $urdu = (int)$urdu;
-    $maths = (int)$maths;
+Route::get('/marksheet/{username}/eng/{english}/ur/{urdu}/math/{maths}', function ($name, $english, $urdu, $maths) {
 
 
-    $total = $english + $urdu + $maths;
-
-    $maxScore = 300; 
-    $percentage = ($total / $maxScore) * 100;
-
-
-    echo "Name: " . $username . "<br>";
-    echo "ID: " . $userid . "<br>";
-    echo "English: " . $english . "<br>";
-    echo "Urdu: " . $urdu . "<br>";
-    echo "Maths: " . $maths . "<br>";
-    echo "Total: " . $total . "<br>";
-    echo "Percentage: " .$percentage. "%";
+    $data = compact('name', "english", 'urdu', "maths");
+    return view('marks')->with($data);
 });
 
 
